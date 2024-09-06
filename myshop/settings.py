656c7021e83097f85d38be8c8752ee56e0b57c2a
Ydyrys Olzhas <im01zhas@icloud.com>
2024-09-06 19:53:15 +0500

    Update

--- myshop/settings.py
+++ myshop/settings.py
@@ -154,3 +154,24 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "debug.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
+
+
