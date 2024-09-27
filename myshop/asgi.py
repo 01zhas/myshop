a83210447b5c0e5c4c8064768136f9dec53cddba
Ydyrys Olzhas <im01zhas@icloud.com>
2024-09-27 10:09:47 +0500

    1

--- myshop/asgi.py
+++ myshop/asgi.py
@@ -1,10 +1,13 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+django.setup()  # Вызовите это до импорта других модулей
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import shop.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+import shop.routing  # Импортируйте после django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
